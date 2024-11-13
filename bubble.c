549c7531c1868aabdee1310cca994857e8bd7fb5
harsh <shivaa@gmail.com>
2024-11-13 14:44:44 +0530

    This is V2 of bubble sort

--- bubble.c
+++ bubble.c
@@ -20,7 +20,7 @@ void printArray(int arr[], int n) {
 }
 
 int main() {
-    int arr[] = {73, 900, 12, 34, 100, 03, 67};
+    int arr[] = {73, 900, 12, 34, 100, 03, 23};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     bubbleSort(arr, n);
